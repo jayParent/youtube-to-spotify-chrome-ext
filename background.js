c0062152a7aa7aaace2e6b08a3f9a24c3c656e2a
Jerome <jeromeparent9@gmail.com>
2020-08-13 20:13:15 -0400

    changed to no-popup

--- background.js
+++ background.js
@@ -1,7 +1,15 @@
+const client_id = "8982911bb26f4732b6fc7012fa25460c";
+const response_type = "token";
+const redirect_uri =
+  "https://njeklmcpajnlhfbnlokoihegcgimoojg.chromiumapp.org/success";
+const scope = "user-library-modify";
+let authorizationUrl = `https://accounts.spotify.com/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}&response_type=${response_type}`;
+
 let rule1 = {
   conditions: [
     new chrome.declarativeContent.PageStateMatcher({
       pageUrl: { hostEquals: "www.youtube.com" },
+      css: ["video"],
     }),
   ],
   actions: [new chrome.declarativeContent.ShowPageAction()],
@@ -10,5 +18,65 @@ let rule1 = {
 chrome.runtime.onInstalled.addListener(function (details) {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([rule1]);
+  });
+});
+
+chrome.browserAction.onClicked.addListener(() => {
+  chrome.identity.launchWebAuthFlow(
+    {
+      url: authorizationUrl,
+      interactive: true,
+    },
+    function (responseUrl) {
+      access_token = responseUrl.slice(responseUrl.indexOf("=") + 1);
+
+      chrome.storage.sync.set({ access_token: access_token }, function () {
+        console.log("Value is set to " + access_token);
+      });
+    }
+  );
+
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.executeScript(tabs[0].id, {
+      file: "contentScript.js",
+    });
+  });
+
+  chrome.runtime.onMessage.addListener(function (
+    request,
+    sender,
+    sendResponse
+  ) {
+    let query1 = request.secondHalf;
+    let query2 = request.firstHalf;
+
+    let options = {
+      url: `https://api.spotify.com/v1/search?q=${query1}&type=track&market=US&limit=5&offset=0`,
+      headers: { Authorization: "Bearer " + access_token },
+      json: true,
+    };
+
+    fetch(options.url, { headers: options.headers })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.tracks.items.length > 0) {
+          let trackId = res.tracks.items[0].id;
+          saveTrackToSpotify(trackId);
+        } else {
+          let options = {
+            url: `https://api.spotify.com/v1/search?q=${query2}&type=track&market=US&limit=5&offset=0`,
+            headers: { Authorization: "Bearer " + access_token },
+            json: true,
+          };
+          fetch(options.url, { headers: options.headers })
+            .then((res) => res.json())
+            .then((res) => {
+              let trackId = res.tracks.items[0].id;
+              saveTrackToSpotify(trackId);
+            })
+            .catch((err) => console.log(err));
+        }
+      })
+      .catch((err) => console.log(err));
   });
 });
---
+++ scripts.js
@@ -0,0 +1,15 @@
+function saveTrackToSpotify(trackId) {
+  let options = {
+    url: "https://api.spotify.com/v1/me/tracks?ids=" + trackId,
+    headers: {
+      Authorization: "Bearer " + access_token,
+    },
+    json: true,
+  };
+
+  fetch(options.url, {
+    headers: options.headers,
+    method: "PUT",
+  })
+    .catch((err) => console.log(err));
+}
